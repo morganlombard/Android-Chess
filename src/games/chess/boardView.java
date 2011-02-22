bef7a269498b497fd467ef7fe8264cf03af3e311
Greg Orlov <gaorlov@gmail.com>
2011-02-22 13:25:07 -0800

    finished movement and pieces

--- src/games/chess/boardView.java
+++ src/games/chess/boardView.java
@@ -1,5 +1,7 @@
 package games.chess;
 
+import java.util.ArrayList;
+
 import games.chess.chessCore.cell;
 import games.chess.chessCore.piece;
 import android.content.Context;
@@ -23,11 +25,15 @@ public class boardView extends View
 	// selected acts as an indicator as to  whether a cell has been selected
 	// if so, the next screen event will be the selection of the move to cell
 	boolean pieceSelected;
-	
+	boolean reset;
 	// Yzero is the top point for the board, as in board is placed at [0, Yzero]
 	private int fromX, fromY, toX, toY, Yzero, width;
-	private chess game;
 	private chessCore core;
+	public void setCore(chessCore core)
+	{
+		if(core != null)
+			this.core = core;
+	}
 	private Resources res;
 	private Canvas canvas;
 	
@@ -41,14 +47,14 @@ public class boardView extends View
 		return y * width / 8 + Yzero;
 	}
 	
-	private void drawBoard(cell[][] board) {
-		canvas.restore();
+	private void drawBoard(cell[][] board)
+	{	
+		super.invalidate();
 		Drawable boardImg = res.getDrawable(R.drawable.board);
 		width =  canvas.getWidth(); 
 		Yzero =  (int)(canvas.getHeight() - width) / 2;
 		boardImg.setBounds(0, Yzero, width, width + Yzero);
 		boardImg.draw(canvas);
-		
 		for(int x = 0; x < 8; x++)
 		{
 			for(int y = 0; y < 8; y++)
@@ -59,6 +65,29 @@ public class boardView extends View
 					Drawable figure = res.getDrawable(piece.getImageResource());
 					figure.setBounds(getBoardX(x), getBoardY(y), getBoardX(x) + width/8, getBoardY(y) + width /8);
 					figure.draw(canvas);
+				}
+			}
+		}
+	}
+	
+	private void drawAvailableMoves(cell[][] board, int x, int y)
+	{
+		piece piece = board[x][y].getPiece();
+		if(piece != null && piece.getPieceColour() == core.getTurn())
+		{
+			Drawable selection = res.getDrawable(R.drawable.selected);
+			selection.setBounds(getBoardX(x), getBoardY(y), getBoardX(x) + width/8, getBoardY(y) + width /8);
+			selection.draw(canvas);
+			
+			ArrayList<cell> availMoves = piece.getAvailableMoves();
+			for(int i = 0; i < availMoves.size(); i ++)
+			{
+				if(piece.isValidMove(availMoves.get(i)))
+				{
+					cell availMove = availMoves.get(i);
+					Drawable circle = res.getDrawable(R.drawable.selectioncircle);
+					circle.setBounds(getBoardX(availMove.getX()), getBoardY(availMove.getY()), getBoardX(availMove.getX()) + width/8, getBoardY(availMove.getY()) + width /8);
+					circle.draw(canvas);
 				}
 			}
 		}
@@ -91,8 +120,9 @@ public class boardView extends View
 			{
 				try
 				{
-					if(core.move(fromX, fromY, toX,toY));
-					this.drawBoard(core.getBoard());
+					core.move(fromX, fromY, toX,toY);
+					pieceSelected = false;
+					return true;
 				}catch( Exception ex)
 				{
 					pieceSelected = false;
@@ -112,23 +142,24 @@ public class boardView extends View
 	protected void onDraw(Canvas canvas) 
 	{		
 		this.canvas = canvas;
-		pieceSelected = false;
-		fromX =-1;
-		fromY = -1;
-		toX = -1;
-		toY = -1;
-		// TODO: change this constructor to be meaningful
-		core = new chessCore();
-		res = getResources();
 		this.drawBoard(core.getBoard());
+		if(pieceSelected)
+			drawAvailableMoves(core.getBoard(), fromX, fromY);
 	}
 	
 	boardView(Context context)
 	{
 		super(context);
-		game = (chess)context;
+		
+		pieceSelected = false;
+		reset = true;
+		fromX =-1;
+		fromY = -1;
+		toX = -1;
+		toY = -1;
+		res = getResources();
+		
 		setFocusable(true);
 		setFocusableInTouchMode(true);
 	}
-	
 }
